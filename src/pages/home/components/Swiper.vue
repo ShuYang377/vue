fcaeeeddd001d260342cd535c0b80eccd26262d6
yangshu <yangshu377@yahoo.com>
2019-06-24 10:48:47 +0800

    add index-icons

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -20,10 +20,10 @@ export default {
             },
             swiperList:[{
               id: '0001',
-              imgUrl:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1561551398&di=23805ac35bb0b5fa407acd6fa4f5f3e0&imgtype=jpg&er=1&src=http%3A%2F%2Fwww.ahstatic.com%2Fphotos%2F2117_de_00_p_2048x1536.jpg'
+              imgUrl:'https://farm8.staticflickr.com/7146/6708815591_ef08beeab4.jpg'
             },{
               id: '0002',
-              imgUrl:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1560956802598&di=3085340848f75fbfed7ac8704501ca41&imgtype=0&src=http%3A%2F%2Fs2.sinaimg.cn%2Fmw690%2F002FJYilzy7mFXNBZRvb1%26690'
+              imgUrl:'https://farm3.staticflickr.com/2338/2203309828_e753de355f.jpg'
             }]
         }
     }
