9861312d20b7b8006eb1a646e9c62e4a9f022798
yangshu <yangshu377@yahoo.com>
2019-06-24 17:17:29 +0800

    finish city.css

---
+++ src/pages/city/components/List.vue
@@ -0,0 +1,125 @@
+<template>
+    <div class="list" ref="wrapper">
+        <div>
+            <div class="area">
+                <div class="title border-topbottom">temporary city</div>
+                <div class="button-list">
+                    <div class = "button-wrapper">
+                        <div class="button">BEIJING</div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="area">
+                <div class="title border-topbottom">famous city</div>
+                <div class="button-list">
+                    <div class = "button-wrapper">
+                        <div class="button">BEIJING</div>
+                    </div>
+                    <div class = "button-wrapper">
+                        <div class="button">NANJING</div>
+                    </div>
+                    <div class = "button-wrapper">
+                        <div class="button">SHANGHAI</div>
+                    </div>
+                    <div class = "button-wrapper">
+                        <div class="button">GUANGZHOU</div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="area">
+                <div class="title border-topbottom">A</div>
+                    <div class="item-list">
+                        <div class="item border-bottom">aa</div>
+                        <div class="item border-bottom">ab</div>
+                        <div class="item border-bottom">ac</div>
+                        <div class="item border-bottom">ad</div>
+                        <div class="item border-bottom">ae</div>
+                        <div class="item border-bottom">af</div>
+                        <div class="item border-bottom">ag</div>
+                    </div>
+            </div>
+
+            <div class="area">
+                <div class="title border-topbottom">B</div>
+                    <div class="item-list">
+                        <div class="item border-bottom">aa</div>
+                        <div class="item border-bottom">ab</div>
+                        <div class="item border-bottom">ac</div>
+                        <div class="item border-bottom">ad</div>
+                        <div class="item border-bottom">ae</div>
+                        <div class="item border-bottom">af</div>
+                        <div class="item border-bottom">ag</div>
+                    </div>
+            </div>
+
+            <div class="area">
+                <div class="title border-topbottom">C</div>
+                    <div class="item-list">
+                        <div class="item border-bottom">aa</div>
+                        <div class="item border-bottom">ab</div>
+                        <div class="item border-bottom">ac</div>
+                        <div class="item border-bottom">ad</div>
+                        <div class="item border-bottom">ae</div>
+                        <div class="item border-bottom">af</div>
+                        <div class="item border-bottom">ag</div>
+                    </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import Bscroll from 'better-scroll'
+export default {
+    name: 'CityList',
+    mounted (){
+        this.scroll = new Bscroll(this.$refs.wrapper)
+    }
+}
+</script>
+
+<style lang = "stylus" scoped>
+    @import '~styles/variables.styl'
+    .border-topbottom
+        &:before
+            border-color: #ccc
+        &:after
+            border-color: #ccc
+    .border-bottom
+        &:before
+            border-color: #ccc
+    .list
+        overflow :hidden
+        position: absolute
+        top: 1.58rem
+        left: 0
+        right: 0
+        bottom: 0
+        .title  
+            line-height : .54rem
+            background : #eee
+            padding-left: .2rem 
+            color: #666
+            font-size: .26rem
+        .button-list
+            overflow: hidden
+            padding: .1rem .6rem .1rem .1rem
+            .button-wrapper
+                float: left
+                width : 33.33%
+                .button
+                    margin: .1rem .1rem
+                    padding: .1rem 0
+                    text-align :center
+                    border: .02rem solid #ccc
+                    border-radius: .06rem
+        .item-list
+            .item
+                line-height :.76rem
+                color:#666
+                padding-left:.2rem
+</style>
+
+
